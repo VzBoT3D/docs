67f1f55fc01aa480009479b59f805de29113f488
Moritz Lutz <moritz.lutz@aeb.com>
2023-03-29 10:08:55 +0200

    mobile update

--- _site/assets/js/imageMapResizer.map.js
+++ _site/assets/js/imageMapResizer.map.js
@@ -1,75 +1,8 @@
-/*! Image Map Resizer (imageMapResizer.min.js ) - v0.5.3 - 2015-01-29
-   *  Desc: Resize HTML imageMap to scaled image.
-   *  Copyright: (c) 2015 David J. Bradshaw - dave@bradshaw.net
-   *  License: MIT
-   */
+/*! Image Map Resizer (imageMapResizer.min.js ) - v1.0.10 - 2019-04-10
+ *  Desc: Resize HTML imageMap to scaled image.
+ *  Copyright: (c) 2019 David J. Bradshaw - dave@bradshaw.net
+ *  License: MIT
+ */
 
-! function() {
-    "use strict";
-
-    function a() {
-      function a() {
-        function a(a) {
-          function c(a) {
-            return a * b[1 === (d = 1 - d) ? "width" : "height"]
-          }
-          var d = 0;
-          return a.split(",").map(Number).map(c).map(Math.floor).join(",")
-        }
-        for (var b = {
-            width: i.width / j.width,
-            height: i.height / j.height
-          }, c = 0; g > c; c++) f[c].coords = a(h[c])
-      }
-
-      function b() {
-        j.onload = function() {
-          (i.width !== j.width || i.height !== j.height) && a()
-        }, j.src = i.src
-      }
-
-      function c() {
-        function b() {
-          clearTimeout(k), k = setTimeout(a, 250)
-        }
-        window.addEventListener ? window.addEventListener("resize", b, !1) : window.attachEvent && window.attachEvent("onresize", b)
-      }
-
-      function d(a) {
-        return a.coords.replace(/ *, */g, ",").replace(/ +/g, ",")
-      }
-      var e = this,
-        f = e.getElementsByTagName("area"),
-        g = f.length,
-        h = Array.prototype.map.call(f, d),
-        i = document.querySelector('img[usemap="#' + e.name + '"]'),
-        j = new Image,
-        k = null;
-      b(), c()
-    }
-
-    function b() {
-      function b(b) {
-        if (!b.tagName) throw new TypeError("Object is not a valid DOM element");
-        if ("MAP" !== b.tagName.toUpperCase()) throw new TypeError("Expected <MAP> tag, found <" + b.tagName + ">.");
-        a.call(b)
-      }
-      return function(a) {
-        switch (typeof a) {
-          case "undefined":
-          case "string":
-            Array.prototype.forEach.call(document.querySelectorAll(a || "map"), b);
-            break;
-          case "object":
-            b(a);
-            break;
-          default:
-            throw new TypeError("Unexpected data type (" + typeof a + ").")
-        }
-      }
-    }
-    "function" == typeof define && define.amd ? define([], b) : "object" == typeof exports ? module.exports = b() : window.imageMapResize = b(), "jQuery" in window && (jQuery.fn.imageMapResize = function() {
-      return this.filter("map").each(a).end()
-    })
-  }();
-  //# sourceMappingURL=imageMapResizer.map
+!function(){"use strict";function r(){function e(){var r={width:u.width/u.naturalWidth,height:u.height/u.naturalHeight},a={width:parseInt(window.getComputedStyle(u,null).getPropertyValue("padding-left"),10),height:parseInt(window.getComputedStyle(u,null).getPropertyValue("padding-top"),10)};i.forEach(function(e,t){var n=0;o[t].coords=e.split(",").map(function(e){var t=1==(n=1-n)?"width":"height";return a[t]+Math.floor(Number(e)*r[t])}).join(",")})}function t(e){return e.coords.replace(/ *, */g,",").replace(/ +/g,",")}function n(){clearTimeout(d),d=setTimeout(e,250)}function r(e){return document.querySelector('img[usemap="'+e+'"]')}var a=this,o=null,i=null,u=null,d=null;"function"!=typeof a._resize?(o=a.getElementsByTagName("area"),i=Array.prototype.map.call(o,t),u=r("#"+a.name)||r(a.name),a._resize=e,u.addEventListener("load",e,!1),window.addEventListener("focus",e,!1),window.addEventListener("resize",n,!1),window.addEventListener("readystatechange",e,!1),document.addEventListener("fullscreenchange",e,!1),u.width===u.naturalWidth&&u.height===u.naturalHeight||e()):a._resize()}function e(){function t(e){e&&(!function(e){if(!e.tagName)throw new TypeError("Object is not a valid DOM element");if("MAP"!==e.tagName.toUpperCase())throw new TypeError("Expected <MAP> tag, found <"+e.tagName+">.")}(e),r.call(e),n.push(e))}var n;return function(e){switch(n=[],typeof e){case"undefined":case"string":Array.prototype.forEach.call(document.querySelectorAll(e||"map"),t);break;case"object":t(e);break;default:throw new TypeError("Unexpected data type ("+typeof e+").")}return n}}"function"==typeof define&&define.amd?define([],e):"object"==typeof module&&"object"==typeof module.exports?module.exports=e():window.imageMapResize=e(),"jQuery"in window&&(window.jQuery.fn.imageMapResize=function(){return this.filter("map").each(r).end()})}();
+//# sourceMappingURL=imageMapResizer.map
